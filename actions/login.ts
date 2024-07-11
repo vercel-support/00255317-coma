1818cfe026b1958fc718d7f16d6d553ea78b4dce
Nick Vigier <nvigier46@gmail.com>
2024-07-11 07:24:51 +0100

    remove middleware

--- actions/login.ts
+++ actions/login.ts
@@ -1,255 +1,95 @@
 // "use server"
-// import { signIn } from "@/auth"
-// import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation"
-// import { getTwoFactorTokenByEmail } from "@/data/two-factor-token"
-// import { getUserByEmail } from "@/data/user.data"
-// import { db } from "@/lib/db"
-// import { sendTwoFactorEmail, sendVerificationEmail } from "@/lib/mail"
-// import { generateTwoFactorToken, generateVerificationToken } from "@/lib/tokens"
-// import { DEFAULT_LOGIN_REDIRECT } from "@/routes"
-// import { LoginSchema, TLogin } from "@/schemas"
-// import { AuthError } from "next-auth"
 
-// export const login = async (
-//     values: TLogin,
-//     callbackUrl?: string | null
-// ) => {
-//     const validateFields = LoginSchema.safeParse(values)
+'use server';
+import { signIn } from '@/auth';
+import { getTwoFactorConfirmationByUserId } from '@/data/two-factor-confirmation';
+import { getTwoFactorTokenByEmail } from '@/data/two-factor-token';
+import { getUserByEmail } from '@/data/user.data';
+import { db } from '@/lib/db';
+import { sendTwoFactorEmail, sendVerificationEmail } from '@/lib/mail';
+import {
+  generateTwoFactorToken,
+  generateVerificationToken,
+} from '@/lib/tokens';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
+import { LoginSchema, TLogin } from '@/schemas';
+import { AuthError } from 'next-auth';
 
-//     if (!validateFields.success) {
-//         return { error: "Campos inválidos" }
-//     }
+export const login = async (values: TLogin, callbackUrl?: string | null) => {
+  const validateFields = LoginSchema.safeParse(values);
 
-//     const { email, password, code } = validateFields.data
+  if (!validateFields.success) {
+    return { error: 'Campos inválidos' };
+  }
 
-//     const existingUser = await getUserByEmail(email!)
+  const { email, password, code } = validateFields.data;
 
-//     if (!existingUser || !existingUser.password || !existingUser.email) {
-//         return { error: "Credenciales inválidas" }
-//     }
+  const existingUser = await getUserByEmail(email!);
 
-//     if (!existingUser.emailVerified) {
-//         const verificationToken = await generateVerificationToken(existingUser.email);
+  if (!existingUser || !existingUser.password || !existingUser.email) {
+    return { error: 'Credenciales inválidas' };
+  }
 
-//         await sendVerificationEmail(verificationToken.email, verificationToken.token, existingUser.name);
-//         return { success: "Correo de verificación enviado" }
+  if (!existingUser.emailVerified) {
+    const verificationToken = await generateVerificationToken(
+      existingUser.email
+    );
 
-//     }
+    await sendVerificationEmail(
+      verificationToken.email,
+      verificationToken.token,
+      existingUser.name
+    );
+    return { success: 'Correo de verificación enviado' };
+  }
 
-//     if (existingUser.isTwoFactorEnabled && existingUser.email) {
-//         if (code) {
-//             const twoFactorToken = await getTwoFactorTokenByEmail(existingUser.email);
-//             if (!twoFactorToken) return { error: "Código inválido" }
-//             if (twoFactorToken.token !== code) return { error: "Código inválido" }
+  if (existingUser.isTwoFactorEnabled && existingUser.email) {
+    if (code) {
+      const [twoFactorToken, existingConfirmation] = await Promise.all([
+        getTwoFactorTokenByEmail(existingUser.email),
+        getTwoFactorConfirmationByUserId(existingUser.id),
+      ]);
 
-//             const hasExpired = new Date(twoFactorToken.expires) < new Date();
-//             if (hasExpired) return { error: "Código expirado" }
+      if (!twoFactorToken) return { error: 'Código inválido' };
+      if (twoFactorToken.token !== code) return { error: 'Código inválido' };
 
-//             await db.twoFactorToken.delete({
-//                 where: {
-//                     id: twoFactorToken.id,
-//                 }
-//             })
-//             const existingConfirmation = await getTwoFactorConfirmationByUserId(existingUser.id);
-//             if (existingConfirmation) {
-//                 await db.twoFactorConfirmation.delete({
-//                     where: {
-//                         id: existingConfirmation.id,
-//                     }
-//                 })
-//             }
+      const hasExpired = new Date(twoFactorToken.expires) < new Date();
+      if (hasExpired) return { error: 'Código expirado' };
 
-//             await db.twoFactorConfirmation.create({
-//                 data: {
-//                     userId: existingUser.id,
-//                 }
-//             })
-//         } else {
-//             const twoFactorToken = await generateTwoFactorToken(existingUser.email);
+      await Promise.all([
+        db.twoFactorToken.delete({ where: { id: twoFactorToken.id } }),
+        existingConfirmation
+          ? db.twoFactorConfirmation.delete({
+              where: { id: existingConfirmation.id },
+            })
+          : Promise.resolve(),
+      ]);
 
-//             await sendTwoFactorEmail(twoFactorToken.email, twoFactorToken.token);
-//             return { twoFactor: true }
-//         }
-//     }
+      await db.twoFactorConfirmation.create({
+        data: { userId: existingUser.id },
+      });
+    } else {
+      const twoFactorToken = await generateTwoFactorToken(existingUser.email);
 
-//     try {
-//         await signIn("credentials", {
-//             email,
-//             password,
-
-//         })
-//     } catch (error) {
-//         console.log('[SIGN_IN_CREDENTIALS]', { error })
-//         if (error instanceof AuthError) {
-//             switch (error.type) {
-//                 case "CredentialsSignin":
-//                     return { error: "Credenciales inválidas" }
-//                 default:
-//                     return { error: "Ha ocurrido un error. Por favor, intenta de nuevo." }
-//             }
-//         }
-//         throw error
-//     }
-// }
-
-"use server"
-import { signIn } from "@/auth"
-import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation"
-import { getTwoFactorTokenByEmail } from "@/data/two-factor-token"
-import { getUserByEmail } from "@/data/user.data"
-import { db } from "@/lib/db"
-import { sendTwoFactorEmail, sendVerificationEmail } from "@/lib/mail"
-import { generateTwoFactorToken, generateVerificationToken } from "@/lib/tokens"
-import { DEFAULT_LOGIN_REDIRECT } from "@/routes"
-import { LoginSchema, TLogin } from "@/schemas"
-import { AuthError } from "next-auth"
-
-// export const login = async (
-//     values: TLogin,
-//     callbackUrl?: string | null
-// ) => {
-//     const validateFields = LoginSchema.safeParse(values)
-
-//     if (!validateFields.success) {
-//         return { error: "Campos inválidos" }
-//     }
-
-//     const { email, password, code } = validateFields.data
-
-//     const existingUser = await getUserByEmail(email!)
-
-//     if (!existingUser || !existingUser.password || !existingUser.email) {
-//         return { error: "Credenciales inválidas" }
-//     }
-
-//     if (!existingUser.emailVerified) {
-//         const verificationToken = await generateVerificationToken(existingUser.email);
-
-//         await sendVerificationEmail(verificationToken.email, verificationToken.token, existingUser.name);
-//         return { success: "Correo de verificación enviado" }
-//     }
-
-//     if (existingUser.isTwoFactorEnabled && existingUser.email) {
-//         if (code) {
-//             const [twoFactorToken, existingConfirmation] = await Promise.all([
-//                 getTwoFactorTokenByEmail(existingUser.email),
-//                 getTwoFactorConfirmationByUserId(existingUser.id)
-//             ]);
-
-//             if (!twoFactorToken) return { error: "Código inválido" }
-//             if (twoFactorToken.token !== code) return { error: "Código inválido" }
-
-//             const hasExpired = new Date(twoFactorToken.expires) < new Date();
-//             if (hasExpired) return { error: "Código expirado" }
-
-//             await Promise.all([
-//                 db.twoFactorToken.delete({
-//                     where: {
-//                         id: twoFactorToken.id,
-//                     }
-//                 }),
-//                 existingConfirmation ? db.twoFactorConfirmation.delete({
-//                     where: {
-//                         id: existingConfirmation.id,
-//                     }
-//                 }) : Promise.resolve()
-//             ]);
-
-//             await db.twoFactorConfirmation.create({
-//                 data: {
-//                     userId: existingUser.id,
-//                 }
-//             })
-//         } else {
-//             const twoFactorToken = await generateTwoFactorToken(existingUser.email);
-
-//             await sendTwoFactorEmail(twoFactorToken.email, twoFactorToken.token);
-//             return { twoFactor: true }
-//         }
-//     }
-
-//     try {
-//         await signIn("credentials", {
-//             email,
-//             password,
-//         })
-//     } catch (error) {
-//         console.log('[SIGN_IN_CREDENTIALS]', { error })
-//         if (error instanceof AuthError) {
-//             switch (error.type) {
-//                 case "CredentialsSignin":
-//                     return { error: "Credenciales inválidas" }
-//                 default:
-//                     return { error: "Ha ocurrido un error. Por favor, intenta de nuevo." }
-//             }
-//         }
-//         throw error
-//     }
-// }
-export const login = async (
-    values: TLogin,
-    callbackUrl?: string | null
-) => {
-    const validateFields = LoginSchema.safeParse(values);
-
-    if (!validateFields.success) {
-        return { error: "Campos inválidos" };
+      await sendTwoFactorEmail(twoFactorToken.email, twoFactorToken.token);
+      return { twoFactor: true };
     }
+  }
 
-    const { email, password, code } = validateFields.data;
-
-    const existingUser = await getUserByEmail(email!);
-
-    if (!existingUser || !existingUser.password || !existingUser.email) {
-        return { error: "Credenciales inválidas" };
+  try {
+    await signIn('credentials', { email, password });
+  } catch (error) {
+    console.log('[SIGN_IN_CREDENTIALS]', { error });
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return { error: 'Credenciales inválidas' };
+        default:
+          return {
+            error: 'Ha ocurrido un error. Por favor, intenta de nuevo.',
+          };
+      }
     }
-
-    if (!existingUser.emailVerified) {
-        const verificationToken = await generateVerificationToken(existingUser.email);
-
-        await sendVerificationEmail(verificationToken.email, verificationToken.token, existingUser.name);
-        return { success: "Correo de verificación enviado" };
-    }
-
-    if (existingUser.isTwoFactorEnabled && existingUser.email) {
-        if (code) {
-            const [twoFactorToken, existingConfirmation] = await Promise.all([
-                getTwoFactorTokenByEmail(existingUser.email),
-                getTwoFactorConfirmationByUserId(existingUser.id)
-            ]);
-
-            if (!twoFactorToken) return { error: "Código inválido" };
-            if (twoFactorToken.token !== code) return { error: "Código inválido" };
-
-            const hasExpired = new Date(twoFactorToken.expires) < new Date();
-            if (hasExpired) return { error: "Código expirado" };
-
-            await Promise.all([
-                db.twoFactorToken.delete({ where: { id: twoFactorToken.id } }),
-                existingConfirmation ? db.twoFactorConfirmation.delete({ where: { id: existingConfirmation.id } }) : Promise.resolve()
-            ]);
-
-            await db.twoFactorConfirmation.create({ data: { userId: existingUser.id } });
-        } else {
-            const twoFactorToken = await generateTwoFactorToken(existingUser.email);
-
-            await sendTwoFactorEmail(twoFactorToken.email, twoFactorToken.token);
-            return { twoFactor: true };
-        }
-    }
-
-    try {
-        await signIn("credentials", { email, password });
-    } catch (error) {
-        console.log('[SIGN_IN_CREDENTIALS]', { error });
-        if (error instanceof AuthError) {
-            switch (error.type) {
-                case "CredentialsSignin":
-                    return { error: "Credenciales inválidas" };
-                default:
-                    return { error: "Ha ocurrido un error. Por favor, intenta de nuevo." };
-            }
-        }
-        throw error;
-    }
+    throw error;
+  }
 };
